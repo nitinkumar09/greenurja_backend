a3dd755ef3c2d398f1e4658ae4c0a180895b8d96
Kapil Krishna Yadav <yadavkapil2336@gmail.com>
2025-04-25 17:09:51 +0530

    fixed error of using cors before the api route in server.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const route = require("./routes/routes");
+app.use(cors());
 app.use(express.json());
 app.use("/api", route);
-app.use(cors());
 
 
 
