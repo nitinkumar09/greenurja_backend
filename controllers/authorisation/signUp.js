6b6d9d578b01622f001e4b9300a03a011d944998
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-16 11:07:13 +0530

    some changes

--- controllers/authorisation/signUp.js
+++ controllers/authorisation/signUp.js
@@ -11,7 +11,7 @@ exports.signUp = async (req, res) => {
             return res.status(400).json({ error: "User already exists" });
         }
 
-        // Create a new user
+        // Create a new user add phone alo
 
         const newUser = new User({
             firstName,
