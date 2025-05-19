1bc038299e3b0c84de0040880a0d22d9cb820500
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-19 10:39:47 +0530

    Change saller api for all saller get for same pin

--- connectDb/conn.js
+++ connectDb/conn.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config();
+console.log(process.env.MONGO_URI);
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(
-    "mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja",
-  )
+  .connect("mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja")
   .then(() => {
     console.log("running succesfully");
   })
--- controllers/seller/getSellerByPin.js
+++ controllers/seller/getSellerByPin.js
@@ -3,19 +3,25 @@ const Seller = require("../../models/Seller");
 exports.getSellerByPin = async (req, res) => {
     try {
         const { pin } = req.params;
+        console.log("Pin received:", pin);
 
-        const seller = await Seller.findOne({ "address.pin": pin });
+        const sellers = await Seller.find({ "address.pin": pin });
+        console.log("Sellers found:", sellers.length);
 
-        if (!seller) {
-            return res.status(404).json({ message: "No seller found with this pincode" });
+        if (!sellers || sellers.length === 0) {
+            return res.status(404).json({ message: "No sellers found with this pincode" });
         }
 
-        return res.json({
+        // Return an array of matched sellers
+        const response = sellers.map((seller) => ({
             name: seller.name,
             description: seller.description,
-            location: seller.address.location,
-        });
+            location: seller.address?.location || "Location not available",
+        }));
+
+        return res.json(response);
     } catch (error) {
+        console.error("Error in getSellerByPin:", error);
         return res.status(500).json({ message: "Server error" });
     }
 };
