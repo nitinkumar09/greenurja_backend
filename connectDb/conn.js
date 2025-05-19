42d6e76d7536c21f0fd4813fe248bce3dc74c261
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-19 11:27:31 +0530

    Add URI Again

--- connectDb/conn.js
+++ connectDb/conn.js
@@ -4,7 +4,7 @@ console.log(process.env.MONGO_URI);
 mongoose.set("strictQuery", true);
 mongoose
   // .connect("mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja")
-  .connect(process.env.MONGO_URI)
+  .connect("mongodb+srv://kumarmongo8865:%40mongo8865@cluster0.we1bjmz.mongodb.net/")
   .then(() => {
     console.log("running succesfully");
   })
