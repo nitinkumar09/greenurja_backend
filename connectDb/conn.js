c3cdd88f17c86dc2376eef8dd01f7cb60e16ba03
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-19 12:38:11 +0530

    add

--- connectDb/conn.js
+++ connectDb/conn.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-require('dotenv').config();
-// console.log(process.env.MONGO_URI);
 mongoose.set("strictQuery", true);
 mongoose
   .connect("mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja")
