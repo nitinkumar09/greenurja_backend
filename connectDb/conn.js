0d3cf92db22c86803218bdcd33d93ea3ffdfef2d
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-21 10:49:35 +0530

    Add URII

--- connectDb/conn.js
+++ connectDb/conn.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 mongoose
-  .connect("mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja")
+  // .connect("mongodb+srv://yadavkapil2336:green@cluster0.tzemhov.mongodb.net/greenurja")
+  .connect("mongodb+srv://kumarmongo8865:%40mongo8865@cluster0.we1bjmz.mongodb.net/greenurja")
   .then(() => {
     console.log("running succesfully");
   })
