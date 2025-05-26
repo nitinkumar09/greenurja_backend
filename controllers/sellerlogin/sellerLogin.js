9f9a0b6b6edb2b392edd655f7dabb591a8d49b3e
nitinkumar09 <kumarnitin83086@gmail.com>
2025-05-26 11:07:42 +0530

    changes model

--- controllers/sellerlogin/sellerLogin.js
+++ controllers/sellerlogin/sellerLogin.js
@@ -1,4 +1,4 @@
-const SellerLogin = require('../../models/PendingSeller');
+const SellerLogin = require('../../models/ApprovedSeller');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
